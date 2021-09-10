16dd4310fb59b04c606514c6cbe639ff291700f0
wojciechzyla <62514065+wojciechzyla@users.noreply.github.com>
2021-09-10 19:15:53 +0200

    threading without authentication

--- img2txt/routes.py
+++ img2txt/routes.py
@@ -8,6 +8,11 @@ from img2txt.convert_to_txt import Pdf2txt, Image2text
 import os
 import cv2
 import base64
+import threading
+import requests
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
 
@@ -16,31 +21,39 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods=['POST'])
-def convert_image():
-    if 'file' not in request.json:
-        message = json.dumps({"info": "No file included"})
-        resp = Response(message, status=406, mimetype='application/json')
-        return resp
-    pdf_b64 = request.json['file']
-    file = base64.b64decode(pdf_b64.encode('utf-8'))
-    file_name = secure_filename(request.json['filename'])
-    if file and allowed_file(file_name):
-        filename = secure_filename(file_name)
-        with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "wb") as f:
-            f.write(file)
-        if file_name.rsplit('.', 1)[1].lower() == 'pdf':
-            preprocess = Pdf2txt(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        else:
-            image = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            preprocess = Image2text([image])
-        result = preprocess.convert()
-        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        message = json.dumps({"file_name": f'{filename}', "text": result})
-        resp = Response(message, status=200, mimetype='application/json')
-        return resp
-    else:
-        message = json.dumps({"info": "Wrong file type"})
-        resp = Response(message, status=406, mimetype='application/json')
-        return resp
+@app.route("/<user_id>", methods=['POST'])
+def convert_image(user_id):
+    data = request.get_json()
 
+    def retrieve_text(**kwargs):
+        URL_PANEL = os.environ.get("URL_PANEL")
+        params = kwargs.get('post_data')
+
+        pdf_b64 = params['file']
+        file = base64.b64decode(pdf_b64.encode('utf-8'))
+        file_name = secure_filename(params['filename'])
+
+        if file and allowed_file(file_name):
+            filename = secure_filename(file_name)
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "wb") as f:
+                f.write(file)
+            if file_name.rsplit('.', 1)[1].lower() == 'pdf':
+                preprocess = Pdf2txt(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            else:
+                image = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                preprocess = Image2text([image])
+            result = preprocess.convert()
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            message = {"file_name": f'{filename}',
+                       "text": result,
+                       "status_code": 200}
+            requests.post(URL_PANEL + "/" + file_name + "/" + str(user_id), json=message)
+        else:
+            message = {"info": "Wrong file type",
+                       "status_code":  406}
+            requests.post(URL_PANEL + "/" + file_name + "/" + str(user_id), json=message)
+
+    thread = threading.Thread(target=retrieve_text, kwargs={'post_data': data})
+    thread.start()
+    return {"info": "accepted"}, 202
